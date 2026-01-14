61bacf06e56aac6f8170ba28281542709430f2d5
thejavalistener <thejavalistener@gmail.com>
2026-01-14 09:35:13 -0300

    Commit automatico

--- src/main/java/thejava/listener/fwkutils/log/MyLogs.java
+++ src/main/java/thejava/listener/fwkutils/log/MyLogs.java
@@ -2,20 +2,35 @@ package thejava.listener.fwkutils.log;
 
 public final class MyLogs
 {
-	private static volatile MyLog INSTANCE;
+    private static volatile MyLog INSTANCE;
 
-	public static MyLog get()
-	{
-		if(INSTANCE==null)
-		{
-			synchronized(MyLogs.class)
-			{
-				if(INSTANCE==null)
-				{
-					INSTANCE=new MyLog("app.log",true);
-				}
-			}
-		}
-		return INSTANCE;
-	}
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (INSTANCE != null) {
+                INSTANCE.close();
+            }
+        }));
+    }
+
+    public static MyLog get()
+    {
+        if (INSTANCE == null)
+        {
+            synchronized (MyLogs.class)
+            {
+                if (INSTANCE == null)
+                {
+                    String file = System.getenv("MYLOG_FILE");
+                    if (file != null && file.isBlank()) file = null;
+
+                    boolean append = Boolean.parseBoolean(
+                        System.getenv().getOrDefault("MYLOG_APPEND", "true")
+                    );
+
+                    INSTANCE = new MyLog(file, append);
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
